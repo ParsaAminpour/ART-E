fad6e2b19d016bd65bd20d185e0420dfd27a40e7
parsa <parsa.aminpour@gmail.com>
2022-09-07 07:17:20 +0430

    resolve problems of complement_script.py

--- MinterBrownie/scripts/complement_scripts.py
+++ MinterBrownie/scripts/complement_scripts.py
@@ -1,40 +1,40 @@
-from email import header
 from brownie import Minter, accounts, network, config, interface
+from requests.exceptions import ConnectionError, Timeout
 from rich import print, pretty, print_json
 from rich.console import Console
 from pathlib import Path
 import sys, requests, json, logging, os, io
 import ipfsApi 
+from textblob import TextBlob 
 from dotenv import load_dotenv, find_dotenv
 from asgiref.sync import async_to_sync, sync_to_async
 console = Console()
 
-@sync_to_async
-def get_metadata_form():
+
+def get_metadata_form(
+    description:str, name:str, ipfs_uri:str
+):
+    if len(description) == 0 or len(name) == 0:
+        raise ValueError("The name or description content is empty")
+    
+    if not ipfs_uri.startswith("https://") or len(ipfs_uri) == 0:
+        raise ValueError("The IPFS URI is invalid or empty")
+
+    blob = TextBlob(description)
     metadata_schema =\
     {
         "title": "DALL-E art",
-        "type": "AI illusions",
-        "properties": {
-            "name": {
-                "type": "string",
-                "description": "Identifies the asset to which this NFT represents"
-            },
-            "description": {
-                "type": "string",
-                "description": "Describes the asset to which this NFT represents"
-            },
-            "image": {
-                "type": "string",
-                "description": "A URI pointing to a resource with mime type image/* representing the asset to which this NFT represents. Consider making any images at a width between 320 and 1080 pixels and aspect ratio between 1.91:1 and 4:5 inclusive."
-            }
-        }
+        "description": description,
+        "image" : ipfs_uri,
+        "attributes" : [
+            {"trait_type" : "polarity", 
+                "value" : str(blob.sentiment.polarity)},
+            {'trait_type' : "subjectivity",
+                "value" : str(blob.sentiment.subjectivity)}
+        ]
     }
     return json.dumps(metadata_schema)
 
-@async_to_sync
-def generate_metadata_based_on_picture_minted(data):
-    pass
 
 @async_to_sync
 async def adding_to_ipfs(token_id:int, image_path:str) -> str:
@@ -60,36 +60,45 @@ async def adding_to_ipfs(token_id:int, image_path:str) -> str:
 
 
 @async_to_sync
-async def adding_to_pinada(CID, metadata:str):
+async def adding_to_pinata(CID, metadata:str):
     if len(CID) != 46: return False
     if CID[:3] != "Qmb": return "invalid CID"
 
     load_dotenv(find_dotenv())
-    pinata_api_key = os.environ.get("PINATA_API_KEY")
-    pinata_api_secret_key = os.environ.get("PINATA_API_SECRET_KEY")
-    pinata_jwt_key = os.environ.get("PINATA_JWY")
+    PINATA_API_KEY = os.environ.get("PINATA_API_KEY")
+    PINATA_API_SECRET_KEY = os.environ.get("PINATA_API_SECRET_KEY")
+    PINATA_JWT_KEY = os.environ.get("PINATA_JWY")
 
-    metadata_schema = json.loads(get_metadata_form)
-    nft_metadata = ""  # Her we will configure metadata based on the minted image extracted from DALL-E model
+    metadata_schema = json.loads(metadata)
 
     pinata_url = "https://api.pinata.cloud/pinning/pinByHash"
     payload = json.dumps({
         'hashToPin' : CID,
-        'pinataMetadata' : nft_metadata
-    })
+        "pinataMetadata": {
+            "name": metadata_schema.get("title"),
+            "keyvalues": {
+                "description": metadata_schema.get("description"),
+                "image": metadata_schema.get("image")
+            }
+        }       
+  })
+
     headers = {
-        'Authorization' : pinata_jwt_key,
-        'content-Type' : 'application/json'
+        'pinata_api_key' : PINATA_API_KEY,
+        'pinata_secret_api_key' : PINATA_API_SECRET_KEY,
+        'Content-Type' : 'application/json'
     }
 
-    response = await requests.request(
-        'POST', pinata_url, data=payload, headers=headers)
-    
+    try:
+        response = await requests.request(
+            'POST', pinata_url, data=payload, headers=headers)
+    except (ConnectionError, Timeout, Exception) as err : return err
+
     if not response.status_code == 200: 
         logging.debug(f"The status code received from pinata request is {response.status_code}")
         return False
     
-    logging.info("IPFS CID uploaded on Pinata successfully")
+    logging.info(f"IPFS CID {CID} uploaded on Pinata successfully")
     return True
 
 

8214b392a56bc9f3a50bb1f00181498f3de4a9e4
parsa <parsa.aminpour@gmail.com>
2022-09-19 11:30:56 -0400

    resolve some insignificant problems

--- backend/minting/views.py
+++ backend/minting/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.core.exceptions import PermissionDenied
 from django.views import View
+from django.http import HttpResponse
+from django.shortcuts import render
+
 
 class Home(View):
     def get(self, request):
-        pass
+        return HttpResponse("<center><h1> The front section has NOT completed yet</h1></center>")
     
-    def post(self, request, *args, **kwargs):
-        pass

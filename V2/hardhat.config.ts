d34b0c1bd12f0b507f0679e17d6fe7050bda9116
Parsa <parsa.aminpour@gmail.com>
2023-11-24 11:27:47 +0330

    Split project to two diff version, one in Brownie framework and another on has deployed in Hardhat gramework

--- V2/hardhat.config.ts
+++ V2/hardhat.config.ts
@@ -1,6 +1,8 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+const INFURA_API_LEY = process.env.WEB3_INFURA_ENDPOINT;
+
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
 };
